e4d974d815f0ed8b2b7d1ce1adcb72e49dadf341
DevPrinceK <princesamuelpks@gmail.com>
2023-08-05 20:50:04 +0000

    update

--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
+from rest_framework import generics, permissions, status
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class OverviewAPI(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            "message": "Welcome to the API!",
+            "description": "This is a REST API for the eVoucher App.",
+        })
