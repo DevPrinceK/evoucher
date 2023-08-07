c2c37ace0e625bfaeda5c7deb3c03749d2319388
DevPrinceK <princesamuelpks@gmail.com>
2023-08-07 13:37:16 +0000

    Update urls.py

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from .views import *
+from . import views
 
+# authentication
 urlpatterns = [
-    path("", OverviewAPI.as_view(), name="api-overview"),
+    # API Overview
+    path("", views.OverviewAPI.as_view(), name="api-overview"),
+    # signup a new user
+    path('sign-up/', views.SignUpAPI.as_view(), name="sign_up"),
+    # login users
+    path('login/', views.LoginAPI.as_view(), name="login"),
+]
+
+urlpatterns += [
+    # Gets all events created by user
+    path("all-events/", views.EventListAPI.as_view(), name="all_events"),
+    # create, update, delete event
+    path("cud-event/", views.CUDEventAPI.as_view(), name="cud_events"),
 ]
