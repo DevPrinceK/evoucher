7a7c784745320430542c903762a278d7e184d7b5
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-02 14:43:41 +0000

    Update decorators.py

--- core/core/utils/decorators.py
+++ core/core/utils/decorators.py
@@ -1,6 +1,35 @@
 from rest_framework import status
 from rest_framework.response import Response
 
+class OrganizerAndAppUserOnly(object):
+    '''Decorator to allow access to both organizers and app users'''
+
+    def __init__(self, original_method):
+        self.original_method = original_method
+
+    def __call__(self, request, *args,  **kwargs):
+        if (request.user.is_authenticated) and (request.user.role == "ORGANIZER" or request.user.role == "APP_USER"):
+            return self.original_method(request, *args, **kwargs)
+        else:
+            return Response({
+                "message": "Access Denied!"
+            }, status=status.HTTP_403_FORBIDDEN)
+
+class OrganizerAndRestaurantOnly(object):
+    '''Decorator to check if user is an event organizer'''
+
+    def __init__(self, original_method):
+        self.original_method = original_method
+
+    def __call__(self, request, *args,  **kwargs):
+        if (request.user.is_authenticated) and (request.user.role == "ORGANIZER"):
+            return self.original_method(request, *args, **kwargs)
+        else:
+            return Response({
+                "message": "Access Denied!"
+            }, status=status.HTTP_403_FORBIDDEN)
+            
+            
 class OrganizerOnly(object):
     '''Decorator to check if user is an event organizer'''
 
